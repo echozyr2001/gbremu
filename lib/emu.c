ff0683d9d78088c7d3e97aed1b1a87aeaefd74b6
echozyr2001 <echo.zyr.2001@gmail.com>
2024-04-10 22:27:51 +0800

    feat: build up basic platform
    edit emu_run to basic platform, and add cpu header and func

--- lib/emu.c
+++ lib/emu.c
@@ -1,6 +1,7 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include <cart.h>
+#include <cpu.h>
 #include <emu.h>
 
 /*
@@ -15,6 +16,8 @@
 */
 
 static emu_context ctx;
+
+void delay(u32 ms) { SDL_Delay(ms); }
 
 int emu_run(int argc, char *argv[]) {
   if (argc < 2) {
@@ -33,6 +36,25 @@ int emu_run(int argc, char *argv[]) {
   printf("SDL initialized...\n");
   TTF_Init();
   printf("TTF initialized...\n");
+
+  cpu_init();
+
+  ctx.running = true;
+  ctx.paused = false;
+  ctx.ticks = 0;
+
+  while (ctx.running) {
+    if (ctx.paused) {
+      delay(10);
+      continue;
+    }
+    if (!cpu_step()) {
+      printf("CPU step failed...\n");
+      return -3;
+    }
+
+    ctx.ticks++;
+  }
 
   return 0;
 }
